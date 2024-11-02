3c3fd475a54265c0e4df2f708979c0b019702149
kn9ck <oknackk@gmail.com>
2024-11-02 14:25:26 -0700

    test fix

--- src/config/firebase.tsx
+++ src/config/firebase.tsx
@@ -1,7 +1,9 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
@@ -12,8 +14,11 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 // Initialize Cloud Firestore and get a reference to the server
 export const db = getFirestore(app);
-// Initialize Firebase Authentication
+
+//Initialize Firebase Authentication
 export const auth = getAuth(app);
